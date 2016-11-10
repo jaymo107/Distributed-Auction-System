61bb62488edee7a4db29b696e828e64af6938cb2
James Davies <jaymo107@gmail.com>
2016-11-10 22:13:43 +0000

    Lookup the rmi

--- BuyerClient.java
+++ BuyerClient.java
@@ -1,3 +1,5 @@
+import java.rmi.Naming;
+
 /**
  * The client which will bid on an auction.
  *
@@ -7,5 +9,12 @@ public class BuyerClient {
 
     public static void main(String[] args) {
 
+        try {
+            AuctionServiceSpec service = (AuctionServiceSpec)
+                    Naming.lookup("rmi://localhost/AuctionService");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
