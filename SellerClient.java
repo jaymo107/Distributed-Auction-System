e904a271e0eb64938dad4995e54707baf46df157
James Davies <jaymo107@gmail.com>
2016-11-10 19:52:30 +0000

    Commented out connection for now

--- SellerClient.java
+++ SellerClient.java
@@ -1,17 +1,16 @@
-import java.net.MalformedURLException;
 import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 
 /**
  * Should allow auctions to be created with a given Item
  * and will allow Buyers to bid on that item in the auction.
+ *
+ * @author JamesDavies
  */
 public class SellerClient {
 
     public static void main(String[] args) throws Exception {
 
-        AuctionServerSpec connection = (AuctionServerSpec) Naming.lookup("rmi://127.0.0.1/auction");
+        //AuctionServerSpec connection = (AuctionServerSpec) Naming.lookup("rmi://127.0.0.1/auction");
 
     }
 }
