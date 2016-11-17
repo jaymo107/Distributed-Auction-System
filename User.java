8e5fed584b025517023b865e34be19669f94f394
James Davies <jaymo107@gmail.com>
2016-11-17 00:05:15 +0000

    User class for user info

---
+++ User.java
@@ -0,0 +1,20 @@
+/**
+ * Created by JamesDavies on 16/11/2016.
+ */
+public class User {
+    private String email;
+    private String name;
+
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
